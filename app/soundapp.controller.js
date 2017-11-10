ece55ef85b6618307435372c771ae8571d5c65ad
Lucas Costa <lucasfrct@gmail.com>
2017-11-09 23:11:19 -0200

    Tesste do modulo

---
+++ app/soundapp.controller.js
@@ -0,0 +1,12 @@
+( function ( ) {
+	"use strict";
+
+	angular
+	.module ( "soundapp" )
+	.controller ( "soundappController", soundappController );
+
+	function soundappController ( ) {
+		this.nome = "Lucas";
+	};
+
+} ) ( );
